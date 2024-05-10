953319f031b00f9f95370092dbc596188b2a9055
LiamOtegui <liamotegui@gmail.com>
2024-05-10 16:14:32 -0300

    inputTodo editing and starting ListTodo/EditTodo components

--- client/src/components/inputTodo.js
+++ client/src/components/inputTodo.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
+import "../App.css"
 
 const InputTodo = () => {
 
@@ -20,22 +21,22 @@ const InputTodo = () => {
     }
 
     return (
-        <div>
-            <h1 className="text-center mt-8 text-3xl font-bold">
-                Pern Todo List
-            </h1>
-            <form className="text-center" onSubmit={onSubmitForm}>
+        <>
+            <h1 className="text-center mt-5 font-bold text-[2rem]">Pern todo list</h1>
+
+            <form className="flex justify-center mt-3" onSubmit={onSubmitForm}>
                 <input
                 type="text"
-                className="appearance-none border rounded mt-[1.5rem] py-2 px-[5rem] text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="text borderborder-gray-500 rounded-md shadow w-[20rem] h-[2rem] py-[1.3rem] px-[1rem]"
                 value={description}
-                onChange={event => setDescription(event.target.value)}/>
-                <button className="bg-green-500 p-[0.4rem] rounded-sm ml-1">
-                    Add
-                </button>
+                onChange={event => setDescription(event.target.value)} />
+                <button
+                className="border border-green-300 bg-green-300 w-[3rem] ml-[1rem] rounded-md">Add</button>
             </form>
-        </div>
+
+        </>
     )
+
 }
 
 export default InputTodo;
