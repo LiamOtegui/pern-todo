cd5da54ba09bdf769feefdc43472b8a55521ac4a
LiamOtegui <liamotegui@gmail.com>
2024-05-16 23:00:54 -0300

    getTodos + deleteTodo functions in ListTodo.js

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const ListTodo = () => {
 
-    const [todos, setDescription] = useState([])
+    const [todos, setTodos] = useState([])
 
     const getTodos = async () => {
         try {
@@ -11,27 +11,38 @@ const ListTodo = () => {
                 headers: { "Content-type": "application/json" }
             })
             const jsonData = await response.json()
-            setDescription(jsonData)
+            setTodos(jsonData)
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+    
+    const deleteTodo = async (id) => {
+        try {
+            const deleted = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            })
+            setTodos(todos.filter(todo => todo.todo_id !== id))
         } catch (error) {
             console.error(error.message);
         }
     }
 
-    // useEffect va a hacer una peticion fetch a la restfulapi (server) con la funcion getTodos cuando se renderice y actualice el componente de ListTodo.
+    // useEffect va a hacer una request fetch a la restfulapi (server) con la funcion getTodos cada vez que se agregue (post) otro "todo".
     useEffect(() => {
         getTodos()
     }, [])
 
     return (
         <div className="mt-10 bg-cyan-400 p-1 rounded-[3rem] mx-[8rem]">
-            <div className="text-[1rem] text-cyan-700 mt- mb-10 mx-[1rem] p-8">
+            <div className="text-[1rem] mt- mb-10 mx-[1rem] p-8">
                 <div className="flex flex-wrap justify-center">
                     {
                         todos.map((todo) => (
-                            <div key={todo.todo_id} className="flex justify-center mx-5 my-5 py-[1rem] border border-cyan-500 text-center rounded-lg shadow-xl bg-cyan-300 w-[15%]">
-                                <div>{todo.description}</div>
-                                <div>Edit</div>
-                                <div>Delete</div>
+                            <div key={todo.todo_id} className="flex justify-center mx-5 my-5 p-[1rem] border border-cyan-500 text-center rounded-lg shadow-xl bg-cyan-300">
+                                <div className="flex items-center">{todo.description}</div>
+                                <button className="flex items-center ml-4 bg-cyan-500 rounded-[0.3rem] text-cyan-200 p-[1rem]">Edit</button>
+                                <button className="flex items-center ml-4 bg-cyan-500 rounded-[0.3rem] text-cyan-200 p-[1rem]" onClick={() => deleteTodo(todo.todo_id)}>Delete</button>
                             </div>
                         ))
                     }
